e1968cbf81e293fabc217449ff843f5625090c77
dmytro.lozyak <d.lozyak@reply.it>
2022-11-23 12:21:58 +0100

    add 3 types of layouts

--- app/src/main/java/com/example/recyclerviewproject/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewproject/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.recyclerviewproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.recyclerviewproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initView()
+    }
+
+
+    private fun initView() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_navigation_no_logged) as NavHostFragment
+        val navController = navHostFragment.navController
+       // navController = findNavController(R.id.nav_host_fragment_activity_navigation_no_logged)
+
     }
 }
